356bc65d473299064bd6ba2ecdbb4ea1566d2173
Ravi <ravinj2003@gmail.com>
2026-07-21 22:39:16 +0530

    did product of array except itself

---
+++ ClassicalQues MB/ProductOfArrayExceptSelf.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+// we can solve this by 3 ways 
+// 1) by counting zeros.
+// 2) by calculating both prefix and suffix product separately then multiplying them
+// 3) by calculatign prefix product seperately, then funneling suffix product, as prefixProd[ ] = prefix*variable(suffix product)
+
+
+public class ProductOfArrayExceptSelf {
+    public static void main(String[] args) {
+
+        // if they ask without division go for prefixSum + suffixSum 
+        int[] nums = {-1,1,0,-3,3}; // 24,12,8,6
+
+        int [] prefixProd = new int[nums.length];
+        Arrays.fill(prefixProd,1);
+
+        for(int i = 1;i<nums.length;i++){
+            prefixProd[i] = prefixProd[i-1]*nums[i-1];
+        } 
+
+        int var = 1; // the job of var is just to track suffix product only
+
+        prefixProd[nums.length-1] = prefixProd[nums.length-1]*var;
+
+        for(int i = nums.length-2;i>=0;i--){
+            var = var*nums[i+1];
+            prefixProd[i]*= var;
+        }
+
+        System.out.println(Arrays.toString(prefixProd));
+        
+    }
+}
